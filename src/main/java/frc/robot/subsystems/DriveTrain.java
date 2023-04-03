278e18fd51f7ae95c7652cad3b07be023737cdb2
Pinecone2112 <116848623+Pinecone2112@users.noreply.github.com>
2023-04-03 16:45:23 -0600

    Update DriveTrain.java

--- src/main/java/frc/robot/subsystems/DriveTrain.java
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -1,9 +1,26 @@
 package frc.robot.subsystems;
 
-import com.revrobotics.CANSparkMaxLowLevel.MotorType;
-import com.revrobotics.CANSparkMax.IdleMode;
 
+import com.kauailabs.navx.frc.AHRS;
+// import com.pathplanner.lib.PathPlannerTrajectory;
+// import com.pathplanner.lib.commands.PPRamseteCommand;
+
+import edu.wpi.first.wpilibj.SPI;
+
+import com.revrobotics.CANSparkMax.IdleMode;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.RunCommand;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.diagnostics.DoubleEntry;
 import frc.helpers.CCSparkMax;
+import frc.helpers.OI;
+import frc.maps.ControlMap;
 import frc.maps.RobotMap;
 
 public class DriveTrain {
@@ -11,4 +28,44 @@ public class DriveTrain {
     CCSparkMax fRight = new CCSparkMax("fRight", "fr", RobotMap.FRONT_RIGHT, MotorType.kBrushless, IdleMode.kBrake, RobotMap.FRONT_RIGHT_REVERSE);
     CCSparkMax bLeft = new CCSparkMax("bLeft", "bl", RobotMap.BACK_LEFT, MotorType.kBrushless, IdleMode.kBrake, RobotMap.BACK_LEFT_REVERSE);
     CCSparkMax bRight = new CCSparkMax("bRight", "br", RobotMap.BACK_RIGHT, MotorType.kBrushless, IdleMode.kBrake, RobotMap.BACK_RIGHT_REVERSE);
+
+    MotorControllerGroup left = new MotorControllerGroup(fLeft, bLeft);
+    MotorControllerGroup right = new MotorControllerGroup(fRight, bRight);
+
+    PIDController gyroControl = new PIDController(0.5, 0, 0);
+    public AHRS gyro = new AHRS(SPI.Port.kMXP);
+
+    public double defaultAccelTime = 0.15;
+    public double slowmodeFactor = 1.0;
+
+    public double currentSpeed = 0.0;
+    public double deltaTime = 0.2;
+
+    /**a
+     * @param targetSpeed The speed that will be accelerated to
+     * @param turnSpeed The turning speed; this one will not accelerate
+     * @param accelTime The time it will take to accelerate to max speed in seconds.
+     */
+
+     public void axisDrive(double targetSpeed, double turnSpeed, double accelTime)
+     {
+        if (accelTime != 0){
+
+        }
+        else 
+        {
+            
+        }
+     }
+
+     public void arcade(double yAxis, double xAxis){
+        double max = 1;
+        left.set(OI.normalize((yAxis - xAxis), -max, max));
+        right.set(OI.normalize((yAxis + xAxis), -max, max));
+     }
+
+     public void balance(double gyroAngle){
+        
+     }
+
 }
